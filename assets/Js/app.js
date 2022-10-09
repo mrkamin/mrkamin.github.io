e1f4d09e4159a677cf8034959a0a8965f0736448
mrkamin <108926875+mrkamin@users.noreply.github.com>
2022-10-09 21:08:04 +0500

    Inatiat html and asset

---
+++ assets/Js/app.js
@@ -0,0 +1,7 @@
+window.onscroll = function() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop >0 ){
+        document.getElementById('navbar').classList.add('scrolled')
+    }else{
+        document.getElementById('navbar').classList.remove('scrolled')
+    }
+}
